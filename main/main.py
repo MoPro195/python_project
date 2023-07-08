8c2010b1913ae6b79614352730254da8f9993bd4
MoPro195 <el-rasatmimahmoud@outlook.de>
2023-07-08 16:45:35 +0200

    First Task

---
+++ main/main.py
@@ -0,0 +1,136 @@
+class BasicCalculationTypes:
+    # Calculates the product of the two numbers, divides it by 2,
+    # and takes the remainder when divided by 7.
+    def calculator(self, number1, number2):
+        if not isinstance(number1, int) or not isinstance(number2, int):
+            raise TypeError("Both numbers must be integers.")
+        return (number1 * number2 // 2) % 7
+
+    """
+      Calculates the sum and count of numbers divisible by 2 or 7 up to the given maximum (exclusive).
+      Raises a TypeError if the input is not an integer.
+      Returns the result formatted as a string.
+      """
+
+    def calc_sum_and_count_all_numbers_div_by_2_or_7(self, max_exclusive):
+        total_sum = 0
+        count = 0
+        if not isinstance(max_exclusive, int):
+            raise TypeError("Numbers must be integers")
+
+        for index in range(max_exclusive + 1):
+            if index % 2 == 0 or index % 2 == 0:
+                count += 1
+                total_sum += index
+
+        return self.format_result_as_string(count, total_sum)
+
+        # Formats the count and sum as a string.
+
+    def format_result_as_string(self, count, total_sum):
+        return "Count: {}, Sum: {}".format(count, total_sum)
+
+    def is_even(self, number):
+        if not isinstance(number, int):
+            raise TypeError("Numbers must be integers")
+        return number % 2 == 0
+
+    def is_odd(self, number):
+        if not isinstance(number, int):
+            raise TypeError("Numbers must be integers")
+        return number % 2 != 0
+
+    def number_as_text(self, number):
+        if not isinstance(number, int):
+            raise TypeError("Numbers must be integers")
+
+        digits_text = []
+        while number > 0:
+            last_digit = number % 10
+            if last_digit == 0:
+                digit_text = "ZERO"
+            elif last_digit == 1:
+                digit_text = "ONE"
+            elif last_digit == 2:
+                digit_text = "TWO"
+            elif last_digit == 3:
+                digit_text = "THREE"
+            elif last_digit == 4:
+                digit_text = "FOUR"
+            elif last_digit == 5:
+                digit_text = "FIVE"
+            elif last_digit == 6:
+                digit_text = "SIX"
+            elif last_digit == 7:
+                digit_text = "SEVEN"
+            elif last_digit == 8:
+                digit_text = "EIGHT"
+            elif last_digit == 9:
+                digit_text = "NINE"
+            else:
+                digit_text = ""
+            digits_text.insert(0, digit_text)
+            number //= 10
+
+        return " ".join(digits_text)
+
+    def calculation_perfect_numbers(self, max_exclusive):
+        if not isinstance(max_exclusive, int):
+            raise TypeError("Numbers must be integers")
+        if max_exclusive < 0:
+            raise ValueError("Numbers must be positive")
+        number_list = []
+        for number in range(1, max_exclusive + 1):
+            sum = 0
+            for divisible_number in range(1, number):
+                if number % divisible_number == 0:
+                    sum += divisible_number
+            if sum == number:
+                number_list.append(sum)
+
+        return number_list
+
+    def calc_primes_up_to(self, max_value):
+        if not isinstance(max_value, int):
+            raise TypeError("Numbers must be integers")
+        if max_value < 2:
+            raise ValueError("Prime numbers start at 2")
+
+        is_prime = [True] * (max_value + 1)
+        primes = []
+
+        for i in range(2, max_value + 1):
+            for j in range(i * i, max_value + 1, i):
+                if is_prime[j]:
+                    is_prime[j] = False
+
+        for i in range(2, max_value + 1):
+            if is_prime[i]:
+                primes.append(i)
+
+        return primes
+
+    def calculate_checksum(self, input_string):
+        # Check if the input value is a string
+        if not isinstance(input_string, str):
+            raise TypeError("Input must be a string")
+        # Check if the string has at least one element
+        if len(input_string) < 1:
+            raise ValueError("String must have at least one element")
+        total_sum = 0
+        # Iterate over the individual digits in the string
+        for position, digit in enumerate(input_string):
+            # Calculate the intermediate sum taking into account the position of the digit
+            total_sum += (position + 1) * int(digit)
+
+        return total_sum % 10
+
+    def from_roman_number(self, roman_number):
+        roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        if not isinstance(roman_number, str):
+            raise TypeError("Input must be a string")
+        if roman_number not in roman_numerals:
+            raise KeyError("The input is not available in the roman ABC")
+
+
+
---
+++ test/BasicCalculationTypesTest.py
@@ -0,0 +1,74 @@
+import unittest
+
+from main.main import BasicCalculationTypes
+
+
+class TestBasicCalculationTypes(unittest.TestCase):
+    def setUp(self):
+        self.calculator = BasicCalculationTypes()
+
+    def test_calculator_with_positive_numbers(self):
+        result = self.calculator.calculator(6, 7)
+        self.assertEqual(result, 0)
+
+    def test_calculator_with_negatives_numbers(self):
+        result = self.calculator.calculator(-5, -5)
+        self.assertEqual(result, 5)
+
+    def test_calculator_with_float_numbers(self):
+        with self.assertRaises(TypeError):
+            self.calculator.calculator(9.0, 8.0)
+
+    def test_calc_sum_and_count_all_numbers_div_by_2_or_7(self):
+        expected_output = "Count: 6, Sum: 30"
+        self.assertEqual(self.calculator.calc_sum_and_count_all_numbers_div_by_2_or_7(10), expected_output)
+
+    def test_calc_sum_and_count_all_numbers_with_float_number(self):
+        with self.assertRaises(TypeError):
+            self.calculator.calc_sum_and_count_all_numbers_div_by_2_or_7(5.0)
+
+    def test_failure_calc_sum_and_count_all(self):
+        expected_output = "Count: 6, Sum: 890"
+        self.assertFalse(self.calculator.calc_sum_and_count_all_numbers_div_by_2_or_7(10) == expected_output)
+
+    def test_normal_is_even(self):
+        self.assertTrue(self.calculator.is_even(10))
+
+    def test_normal_is_odd(self):
+        self.assertTrue(self.calculator.is_odd(7))
+
+    def test_number_as_text_normality(self):
+        expected_output = "FOUR TWO"
+        self.assertEqual(self.calculator.number_as_text(42), expected_output)
+
+    def test_calculation_perfect_numbers_function(self):
+        expected_output = [6, 28, 496]
+        self.assertEqual(self.calculator.calculation_perfect_numbers(1000), expected_output)
+
+    def test_failure_calculation_perfect_numbers_function(self):
+        with self.assertRaises(ValueError):
+            self.calculator.calculation_perfect_numbers(-100)
+
+    def test_calc_primes_up_to(self):
+        expected_output = [2, 3, 5, 7, 11, 13, 17, 19]
+        self.assertEqual(expected_output, self.calculator.calc_primes_up_to(20))
+
+    def test_normal_function_calculation_checksum(self):
+        self.assertEqual(self.calculator.calculation_checksum("11111"), 5)
+
+    def test_wrong_type_calculation_checksum(self):
+        with self.assertRaises(TypeError):
+            self.calculator.calculation_checksum(100)
+
+    def test_empty_input_calculation_checksum(self):
+        with self.assertRaises(ValueError):
+            self.calculator.calculation_checksum("")
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
