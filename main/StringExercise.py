f7cdaea4a3cfb263a0a150e3f8a0e10a8cf4f178
MoPro195 <el-rasatmimahmoud@outlook.de>
2023-07-23 18:09:25 +0200

    update

--- main/StringExercise.py
+++ main/StringExercise.py
@@ -67,3 +67,96 @@ class StringExercise:
         for i, char in enumerate(reversed(text)):
             decimal_value += hex_digits.index(char) * (16 ** i)
         return decimal_value
+
+    """
+    Joins a list of strings with a delimiter and returns the resulting string.
+
+    Args:
+        values (list): A list of strings to be joined.
+        delimiter (str): The delimiter string to be inserted between the strings.
+
+    Returns:
+        str: The string resulting from joining the elements with the specified delimiter.
+    """
+
+    @staticmethod
+    def join(values: list, delimiter: str) -> str:
+        if not values or not delimiter:
+            raise ValueError("values and delimiter must not be empty")
+        list_to_str = values[0]
+        for index in range(1, len(values)):
+            list_to_str += delimiter + values[index]
+
+        return list_to_str
+
+    """
+    Reverses the characters in a string and returns the reversed string.
+
+    Args:
+        text (str): The input string whose characters are to be reversed.
+
+    Returns:
+        str: The reversed string.
+    """
+
+    @staticmethod
+    def reverse(text: str) -> str:
+        if not text:
+            raise ValueError("Empty string can not be return")
+        if len(text) == 1:
+            return text
+        reversed_text = ""
+        for index in range(len(text) - 1, -1, -1):
+            reversed_text += text[index]
+
+        return reversed_text
+
+    """
+    Checks if a given string is a palindrome, ignoring spaces and punctuation marks.
+
+    Args:
+        text (str): The input string to be checked.
+
+    Returns:
+        bool: True if the input string is a palindrome, False otherwise.
+
+    Raises:
+        ValueError: If the input string is empty.
+    """
+
+    @staticmethod
+    def is_palindrome(text: str) -> bool:
+        if not text:
+            raise ValueError("Empty string can not be checked")
+        cleaned_text = ""
+        for char in text:
+            if char.isalnum():
+                cleaned_text += char
+
+        return cleaned_text.upper() == StringExercise.reverse(cleaned_text.upper())
+
+    """
+    Check if a given string contains no duplicate characters, ignoring the case of letters.
+    
+    Args:
+        text (str): The input string to be checked.
+        
+    Returns:
+        bool: True if the string contains no duplicate characters, False otherwise.
+        
+    Raises:
+        ValueError: If the input string is empty.
+    """
+
+    @staticmethod
+    def check_no_duplicate_chars(text: str) -> bool:
+        if not text:
+            raise ValueError("Empty string can not be checked")
+        if len(text) == 1:
+            return True
+        text = text.lower()
+        for index, char in enumerate(text):
+            if char.isalpha() and text[:index].find(char) != -1:
+                return False
+
+        return True
--- test/StringExerciseTest.py
+++ test/StringExerciseTest.py
@@ -35,6 +35,47 @@ class MyTestCase(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.str_object.hex_to_decimal("FNMBEWW")
 
+    def test_join_normally_function(self):
+        self.assertEqual(self.str_object.join(["hello", "world", "message"], "+++"), "hello+++world+++message")
+
+    def test_join_with_empty_list_or_empty_delimiter(self):
+        with self.assertRaises(ValueError):
+            self.str_object.join([], "+++")
+        with self.assertRaises(ValueError):
+            self.str_object.join(["hello", "world", "message"], "")
+
+    def test_reverse_normally_function(self):
+        self.assertEqual(self.str_object.reverse("ABCD"), "DCBA")
+
+    def test_reverse_with_one_char(self):
+        self.assertEqual(self.str_object.reverse("A"), "A")
+
+    def test_reverse_with_empty_str(self):
+        with self.assertRaises(ValueError):
+            self.str_object.reverse("")
+
+    def test_is_palindrome_normally_function(self):
+        self.assertTrue(self.str_object.is_palindrome("Otto"))
+
+    def test_is_palindrome_with_not_palindrome_text(self):
+        self.assertFalse(self.str_object.is_palindrome("diana"))
+
+    def test_is_palindrome_with_empty_input(self):
+        with self.assertRaises(ValueError):
+            self.str_object.is_palindrome("")
+
+    def test_is_palindrom_with_sentence(self):
+        self.assertTrue(self.str_object.is_palindrome("Dreh mal am Herd."))
+
+    def test_check_no_duplicate_normally_function(self):
+        self.assertFalse(self.str_object.check_no_duplicate_chars("Otto"))
+
+    def test_check_no_duplicate_with_one_char(self):
+        self.assertTrue(self.str_object.check_no_duplicate_chars("Micha"))
+
+    def test_check_no_duplicate_with_one_char_element(self):
+        self.assertTrue(self.str_object.check_no_duplicate_chars("A"))
+
 
 if __name__ == '__main__':
     unittest.main()
